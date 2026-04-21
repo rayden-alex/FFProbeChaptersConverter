150e3c3917d696c2d3836e2dad570ef078632f6a
rayden <rayden@tut.by>
2026-04-21 08:31:36 +0300

    Fixed echo message for out file in the cmd-scripts.

--- FFProbeChaptersConverter_CMD.cmd
+++ FFProbeChaptersConverter_CMD.cmd
@@ -6,7 +6,7 @@ set out_file=%in_file:~0,-6%.cmd"
 
 set "_ffprobe=c:\Rip\ffmpeg\bin\ffprobe.exe -hide_banner  -v error "
 
-echo Creating %in_file%
+echo [INFO] Creating %out_file%
 
 %_ffprobe% -i %in_file% -of json -show_format -show_streams -show_chapters | ^
 java -XX:AOTCache=.\build\libs\FFProbeChaptersConverter.aot -XX:+UseCompactObjectHeaders -jar .\build\libs\FFProbeChaptersConverter.jar  -f CMD > ^
